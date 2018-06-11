40de86867a799f7f5254ca1f562688bb5c1fad85
Tom <tom@example.org>
2018-06-11 02:04:57 -0700

    Dziala!

--- src/main/java/pl/training/helloworld/Application.java
+++ src/main/java/pl/training/helloworld/Application.java
@@ -12,10 +12,13 @@ public class Application {
 
         EntityTransaction transaction = entityManager.getTransaction();
 
+        transaction.begin();
+
         Account account = new Account();
-        account.setId(1L);
-        account.setNumber("0000000001");
-        account.setBalance(1_000);
+        account.setId(2L);
+        account.setNumber("0000000002");
+        account.setBalance(2_000);
+        entityManager.persist(account);
 
         transaction.commit();
 
