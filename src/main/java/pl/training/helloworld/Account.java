0d5d4a5b1b2122e044b0bea54a656e192eb3fc13
Tom <tom@example.org>
2018-06-11 03:56:11 -0700

    Lombok magic

--- src/main/java/pl/training/helloworld/Account.java
+++ src/main/java/pl/training/helloworld/Account.java
@@ -1,13 +1,11 @@
 package pl.training.helloworld;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Objects;
 
-@TableGenerator(name="Custom_generator",
-table="sequence",
-pkColumnName = "name",
-valueColumnName = "value",allocationSize = 10)
-@Table(name="accounts")
+@Data
 @Entity
 public class Account {
     @GeneratedValue(strategy = GenerationType.TABLE, generator = "Custom_generator")
@@ -18,56 +16,4 @@ public class Account {
     private long balance;
     @Enumerated(EnumType.ORDINAL)
     private AccountType type;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public long getBalance() {
-        return balance;
-    }
-
-    public void setBalance(long balance) {
-        this.balance = balance;
-    }
-
-    public AccountType getType() {
-        return type;
-    }
-
-    public void setType(AccountType type) {
-        this.type = type;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Account account = (Account) o;
-        return balance == account.balance &&
-                Objects.equals(number, account.number);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(number, balance);
-    }
-
-    @Override
-    public String toString() {
-        return "Account{" +
-                "id=" + id +
-                ", number='" + number + '\'' +
-                ", balance=" + balance +
-                '}';
-    }
 }
